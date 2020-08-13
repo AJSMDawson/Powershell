9481daf6940ee7a94cc41ebcdf7b21ece1ac456d
Alex Dawson <dawsona@alexsdbmacbook.dboone.local>
2020-08-13 13:07:49 -0400

    Added comment about Exchange

--- DisableADUsers.ps1
+++ DisableADUsers.ps1
@@ -1,5 +1,5 @@
 #Disable, Move, and remove groups from users in a CSV
-#CSV should have UserName first, then the usernames needed. 
+#CSV should have UserName first, then the usernames needed.
 
 Import-Module ActiveDirectory
 $list = "PATH TO CSV"
@@ -9,6 +9,7 @@ $DisabledOU = "DISABLED OU HERE"
 Import-CSV $list | ForEach-Object{
   $user = Get-ADUser -Identity $_.UserName -Properties MemberOf
   Disable-ADAccount -Identity $_.UserName
+  #Comment/Remove next line if you do not disable Exchange mailboxes
   Disable-Mailbox -Identity $_.UserName
   $user | Move-ADObject -TargetPath $disabledUsersOU
   ForEach ($group in ($user | Select-Object -ExpandProperty MemberOf))
