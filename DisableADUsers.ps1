bed2c6c2dab34c030f665281c6e0f776dfee7422
AJSMDawson <69637096+AJSMDawson@users.noreply.github.com>
2020-08-13 12:51:29 -0400

    Create DisableADUsers.ps1

---
+++ DisableADUsers.ps1
@@ -0,0 +1,20 @@
+#Disable, Move, and remove groups from users in a CSV
+#CSV should have UserName first, then the usernames needed. 
+
+Import-Module Active Directory
+$list = "PATH TO CSV"
+$DisabledOU = "DISABLED OU HERE"
+
+
+Import-CSV $list | ForEach-Object{
+  $user = Get-ADUser -Identity $_.UserName -Properties MemberOf
+  Disable-ADAccount -Identity $_.UserName
+  $user | Move-ADObject -TargetPath $disabledUsersOU
+  ForEach ($group in ($user | Select-Object -ExpandProperty MemberOf))
+	{
+		Remove-ADGroupMember -Identity $group -Members $user
+	}
+
+
+
+}
